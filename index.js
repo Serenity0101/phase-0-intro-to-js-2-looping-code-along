c2b30062e06be4d21782f4c215247629a713723d
Serenity Parker <sarenity.parker01@gmail.com>
2023-03-18 06:10:30 -0500

    stuck

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+
+   const names = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(names, event) { 
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+console.log(writeCards(names, "birthday"))
+        
+for (let countDown = 0; countDown < 10; countDown = countDown + 1) {
+    console.log("This for a for loop");
+}
